182082f6898afe2123e99fce425a1da05f9628d7
ath31st <ath31st@yandex.ru>
2024-07-15 11:09:26 +0300

    update Routing.kt with user logic

--- backend/src/main/kotlin/doma/sidim/plugins/Routing.kt
+++ backend/src/main/kotlin/doma/sidim/plugins/Routing.kt
@@ -1,9 +1,14 @@
 package doma.sidim.plugins
 
+import doma.sidim.dto.UserRegisterDto
+import doma.sidim.model.User
+import doma.sidim.repository.UserRepository
+import doma.sidim.service.UserService
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
 import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -16,9 +21,36 @@ fun Application.configureRouting() {
             call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
+    val userService = UserService(UserRepository())
+
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        post("/users") {
+            val userDto = call.receive<UserRegisterDto>()
+            val id = userService.createUser(userDto)
+            call.respond(HttpStatusCode.Created, id)
+        }
+
+        get("/users/{id}") {
+            val id = call.parameters["id"]?.toLong() ?: throw IllegalArgumentException("Invalid ID")
+            val user = userService.getUserById(id)
+            if (user != null) {
+                call.respond(HttpStatusCode.OK, user)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+
+        put("/users/{id}") {
+            val id = call.parameters["id"]?.toLong() ?: throw IllegalArgumentException("Invalid ID")
+            val user = call.receive<User>()
+            userService.updateUser(id, user)
+            call.respond(HttpStatusCode.OK)
+        }
+
+        delete("/users/{id}") {
+            val id = call.parameters["id"]?.toLong() ?: throw IllegalArgumentException("Invalid ID")
+            userService.deleteUserById(id)
+            call.respond(HttpStatusCode.OK)
         }
     }
 }
