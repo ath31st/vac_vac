3f27d6f792d655f9d7ca656e1e65ff77448e0eae
ath31st <ath31st@yandex.ru>
2024-07-15 09:17:28 +0300

    clean and config HTTP

--- src/main/kotlin/doma/sidim/plugins/HTTP.kt
+++ src/main/kotlin/doma/sidim/plugins/HTTP.kt
@@ -4,19 +4,14 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.response.*
 
 fun Application.configureHTTP() {
     install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        allowHeaders { true }
+        HttpMethod.DefaultMethods.forEach { allowMethod(it) }
+        allowNonSimpleContentTypes = true
     }
     install(DefaultHeaders) {
-        header("X-Engine", "Ktor") // will send this header with each response
+        header("X-Engine", "Ktor")
     }
 }
