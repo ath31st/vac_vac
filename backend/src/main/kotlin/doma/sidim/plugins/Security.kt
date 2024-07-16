4e7857fc9536b628a4f60816d74d869adfa30111
ath31st <ath31st@yandex.ru>
2024-07-16 08:32:21 +0300

    rework Security.kt

--- backend/src/main/kotlin/doma/sidim/plugins/Security.kt
+++ backend/src/main/kotlin/doma/sidim/plugins/Security.kt
@@ -1,20 +1,31 @@
 package doma.sidim.plugins
 
 import doma.sidim.service.UserService
+import doma.sidim.util.JwtConfig
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.response.*
 
 fun Application.configureSecurity(userService: UserService) {
     authentication {
-        basic(name = "auth") {
+        jwt("auth-jwt") {
             realm = "Ktor Server"
-            validate { credentials ->
-                val user = userService.authenticate(credentials.name, credentials.password)
-                if (user != null) {
-                    UserIdPrincipal(user.email)
+            verifier(JwtConfig.verifier)
+            validate { credential ->
+                val email = credential.payload.getClaim("email").asString()
+                if (email != null && userService.getUserByEmail(email) != null) {
+                    JWTPrincipal(credential.payload)
                 } else {
                     null
                 }
+            }
+            challenge { _, _ ->
+                call.respond(
+                    HttpStatusCode.Unauthorized,
+                    mapOf("message" to "Token is not valid or has expired")
+                )
             }
         }
     }
