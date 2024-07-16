6b673722805cce1a036e68e663289a786e14aa34
ath31st <ath31st@yandex.ru>
2024-07-16 07:25:17 +0300

    add bcrypt dependency

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("at.favre.lib:bcrypt:0.10.2")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
