4bc1027f347f1d14fcea137429f9e14f2ffa6051
ath31st <ath31st@yandex.ru>
2024-07-17 06:38:58 +0300

    add VacancyService

---
+++ backend/src/main/kotlin/doma/sidim/service/VacancyService.kt
@@ -0,0 +1,31 @@
+package doma.sidim.service
+
+import doma.sidim.dto.VacancyCreationDto
+import doma.sidim.model.Vacancy
+import doma.sidim.repository.VacancyRepository
+import doma.sidim.util.EducationLevels
+import doma.sidim.util.EnglishLevels
+import doma.sidim.util.Tags
+
+class VacancyService(private val vacancyRepository: VacancyRepository) {
+    fun createVacancy(dto: VacancyCreationDto, creatorId: Long): Long {
+        val vacancy = Vacancy(
+            title = dto.title,
+            description = dto.description,
+            englishLevel = EnglishLevels.entries[dto.englishLevelId],
+            grade = EducationLevels.entries[dto.gradeId],
+            tags = Tags.fromSetIds(dto.tags),
+            isVisible = true,
+            creatorId = creatorId,
+        )
+        return vacancyRepository.create(vacancy)
+    }
+
+    fun getVacancy(id: Long): Vacancy? {
+        return vacancyRepository.read(id)
+    }
+
+    fun getAllActiveVacancies(): List<Vacancy> {
+        return vacancyRepository.getActiveVacancies()
+    }
+}
