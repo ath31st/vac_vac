4663069d016c795ca1dbb53e49479e7b1b1fb154
ath31st <ath31st@yandex.ru>
2024-07-22 14:15:04 +0300

    add empty Vacancies.jsx page

---
+++ frontend/src/pages/Vacancies.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+
+`
+const Vacancies = () => {
+  return (
+    <Title>
+      Vacancies
+    </Title>
+  )
+}
+
+export default Vacancies
