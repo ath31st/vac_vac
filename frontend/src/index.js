5c7e5488245ede01c55af9337ad443b45715a412
= <=>
2024-07-22 08:28:43 +0300

    add global style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        min-height: 100vh;
+        background: radial-gradient(#b7b7b7, #484848);
+        display: grid;
+        place-content: center;
+        text-align: center;
+    }
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App/>
   </React.StrictMode>,
 )
