b53de626b733dabc038e0652b694ac79c3cdfbec
ath31st <ath31st@yandex.ru>
2024-07-22 14:15:17 +0300

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Signup from './pages/Signup'
 import NotFoundPage from './pages/NotFoundPage'
+import Layout from './components/Layout'
+import Vacancies from './pages/Vacancies'
+import HomePage from './pages/HomePage'
 
 function App () {
   return (
     <div className="App">
       <Routes>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="*" element={<NotFoundPage/>}/>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/vacancies" element={<Vacancies/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Route>
       </Routes>
     </div>
   )
