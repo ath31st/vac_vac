533d24d5500d16f912f8812e996dec77b6159687
ath31st <ath31st@yandex.ru>
2024-07-22 13:55:24 +0300

    realize routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import Signup from './pages/Signup'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App () {
   return (
     <div className="App">
-      <Signup/>
+      <Routes>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="*" element={<NotFoundPage/>}/>
+      </Routes>
     </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { createGlobalStyle } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
 const GlobalStyle = createGlobalStyle`
     *,
@@ -24,7 +25,9 @@ const GlobalStyle = createGlobalStyle`
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <GlobalStyle/>
-    <App/>
+    <BrowserRouter>
+      <GlobalStyle/>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>,
 )
