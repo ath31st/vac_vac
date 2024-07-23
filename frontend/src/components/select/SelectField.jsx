9133ed454e185848a1df4cb9583bbcf2a111152d
ath31st <ath31st@yandex.ru>
2024-07-23 10:01:16 +0300

    add SelectField.jsx component

---
+++ frontend/src/components/select/SelectField.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledSelect = styled.select`
+    margin: 10px 0;
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+`
+
+const SelectField = ({ name, value, onChange, options }) => {
+  return (
+    <StyledSelect name={name} value={value} onChange={onChange}>
+      <option value="" disabled>Select a role</option>
+      {options.map((option) => (
+        <option key={option.id} value={option.id}>
+          {option.name}
+        </option>
+      ))}
+    </StyledSelect>
+  )
+}
+
+export default SelectField
