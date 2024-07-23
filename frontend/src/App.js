40fb83ad32a856531b947ffbbaab801d320824e0
ath31st <ath31st@yandex.ru>
2024-07-23 09:09:05 +0300

    add Login.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import NotFoundPage from './pages/NotFoundPage'
 import Layout from './components/Layout'
 import Vacancies from './pages/Vacancies'
 import HomePage from './pages/HomePage'
+import Login from './pages/Login'
 
 function App () {
   return (
@@ -12,6 +13,7 @@ function App () {
         <Route path="/" element={<Layout/>}>
           <Route index element={<HomePage/>}/>
           <Route path="/signup" element={<Signup/>}/>
+          <Route path="/login" element={<Login/>}/>
           <Route path="/vacancies" element={<Vacancies/>}/>
           <Route path="*" element={<NotFoundPage/>}/>
         </Route>
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    background: #b7b7b7;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+`
+
+const Input = styled.input`
+    margin: 10px 0;
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+`
+
+const Button = styled.button`
+    padding: 10px 20px;
+    font-size: 16px;
+    color: white;
+    background-color: #007BFF;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-top: 10px;
+
+    &:hover {
+        background-color: #0056b3;
+    }
+`
+
+const ErrorMessage = styled.p`
+    color: red;
+    font-size: 14px;
+`
+
+const Login = () => {
+  const [formState, setFormState] = useState({
+    email: '',
+    password: '',
+  })
+
+  const [error, setError] = useState('')
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormState({
+      ...formState,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (formState.email === '') {
+      setError('Email is required')
+    } else if (formState.password === '') {
+      setError('Password is required')
+    } else {
+      alert('Submited')
+    }
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formState.email}
+          onChange={handleChange}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formState.password}
+          onChange={handleChange}
+        />
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+        <Button
+          type="submit"
+          onClick={handleSubmit}
+        >Sign In</Button>
+      </Form>
+    </Container>
+  )
+}
+
+export default Login
