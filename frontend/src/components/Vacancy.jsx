490c1f7c423314b6c892825679dd06acfdbd319b
ath31st <ath31st@yandex.ru>
2024-07-24 09:31:07 +0300

    add Vacancy.jsx

---
+++ frontend/src/components/Vacancy.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    border: 1px solid #ccc;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const VacancyTitle = styled.h3`
+    margin: 0;
+`
+
+const VacancyDescription = styled.p`
+    margin: 0;
+`
+
+const ResponseStatus = styled.span`
+    color: green;
+`
+
+const Vacancy = ({ title, description }) => {
+  return (
+    <Container>
+      <div>
+        <VacancyTitle>{title}</VacancyTitle>
+        <VacancyDescription>{description}</VacancyDescription>
+      </div>
+      <ResponseStatus>✔ You responded</ResponseStatus>
+    </Container>
+  )
+}
+
+export default Vacancy
