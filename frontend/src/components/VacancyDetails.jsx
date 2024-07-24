08c5634978c70e870f5dd785ee44ef735e082b39
ath31st <ath31st@yandex.ru>
2024-07-24 10:30:39 +0300

    add VacancyDetails.jsx

---
+++ frontend/src/components/VacancyDetails.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    padding: 20px;
+    border-left: 1px solid #ccc;
+    width: 50%;
+`
+
+const VacancyTitle = styled.h2`
+    margin: 0 0 20px;
+`
+
+const VacancyDescription = styled.p`
+    background-color: #e0e0e0;
+    padding: 20px;
+    margin: 0 0 20px;
+`
+
+const Tags = styled.div`
+    margin: 20px 0;
+`
+
+const Tag = styled.span`
+    display: inline-block;
+    margin: 0 10px 10px 0;
+    padding: 5px 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+`
+
+const EnglishGradeLvls = styled.p`
+
+`
+
+const VacancyDetails = ({ vacancy }) => {
+  return (
+    <Container>
+      <VacancyTitle>{vacancy.title}</VacancyTitle>
+      <EnglishGradeLvls>English lvl: {vacancy.englishLevel}</EnglishGradeLvls>
+      <EnglishGradeLvls>Grade: {vacancy.grade}</EnglishGradeLvls>
+      <VacancyDescription>{vacancy.description}</VacancyDescription>
+      <h3>Contacts</h3>
+      <Tags>
+        <Tag>Vacancy tag</Tag>
+        <Tag>Vacancy tag</Tag>
+        <Tag>Vacancy tag</Tag>
+        <Tag>Vacancy tag</Tag>
+        <Tag>Vacancy tag</Tag>
+        <Tag>Vacancy tag</Tag>
+      </Tags>
+      <button>Cancel response</button>
+    </Container>
+  )
+}
+
+export default VacancyDetails
