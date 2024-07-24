ff76a8f362e7d46988ef60de4447bd088342523e
ath31st <ath31st@yandex.ru>
2024-07-24 09:31:34 +0300

    update Vacancies.jsx

--- frontend/src/pages/Vacancies.jsx
+++ frontend/src/pages/Vacancies.jsx
@@ -1,13 +1,34 @@
 import styled from 'styled-components'
+import Vacancy from '../components/Vacancy'
+import Sidebar from '../components/Sidebar'
 
-const Title = styled.h1`
-
+const Container = styled.div`
+    display: flex;
 `
+
+const MainContent = styled.div`
+    flex-grow: 1;
+    padding: 20px;
+`;
+
+const VacanciesContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
+
 const Vacancies = () => {
   return (
-    <Title>
-      Vacancies
-    </Title>
+    <Container>
+      <Sidebar />
+      <MainContent>
+        <VacanciesContainer>
+          <Vacancy title="Vacancy title 1" description="Vacancy short description (first 50 characters)..." />
+          <Vacancy title="Vacancy title 2" description="Vacancy short description (first 50 characters)..." />
+          <Vacancy title="Vacancy title 3" description="Vacancy short description (first 50 characters)..." />
+        </VacanciesContainer>
+      </MainContent>
+    </Container>
   )
 }
 
