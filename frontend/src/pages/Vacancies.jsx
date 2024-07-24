4a583f5204c583a7312266d32e6f0820c354181a
ath31st <ath31st@yandex.ru>
2024-07-24 10:31:04 +0300

    update Vacancies.jsx

--- frontend/src/pages/Vacancies.jsx
+++ frontend/src/pages/Vacancies.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import Vacancy from '../components/Vacancy'
 import Sidebar from '../components/Sidebar'
+import { useState } from 'react'
+import VacancyDetails from '../components/VacancyDetails'
 
 const Container = styled.div`
     display: flex;
@@ -8,25 +10,62 @@ const Container = styled.div`
 
 const MainContent = styled.div`
     flex-grow: 1;
+    display: flex;
     padding: 20px;
-`;
+`
 
 const VacanciesContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 20px;
-`;
+`
 
 const Vacancies = () => {
+  const [selectedVacancy, setSelectedVacancy] = useState(null)
+
+  const vacancies = [
+    {
+      id: 1,
+      title: 'Vacancy title 1',
+      description: 'Vacancy short description (first 50 characters)...',
+      englishLevel: 'eng 1',
+      grade: 'grade 1',
+    },
+    {
+      id: 2,
+      title: 'Vacancy title 2',
+      description: 'Vacancy short description (first 50 characters)...',
+      englishLevel: 'eng 2',
+      grade: 'grade 2',
+    },
+    {
+      id: 3,
+      title: 'Vacancy title 3',
+      description: 'Vacancy short description (first 50 characters)...',
+      englishLevel: 'eng 3',
+      grade: 'grade 3',
+    },
+  ]
+
+  const handleVacancyClick = (vacancy) => {
+    setSelectedVacancy(vacancy)
+  }
+
   return (
     <Container>
-      <Sidebar />
+      <Sidebar/>
       <MainContent>
         <VacanciesContainer>
-          <Vacancy title="Vacancy title 1" description="Vacancy short description (first 50 characters)..." />
-          <Vacancy title="Vacancy title 2" description="Vacancy short description (first 50 characters)..." />
-          <Vacancy title="Vacancy title 3" description="Vacancy short description (first 50 characters)..." />
+          {vacancies.map((vacancy) => (
+            <Vacancy
+              key={vacancy.id}
+              title={vacancy.title}
+              description={vacancy.description}
+              onClick={() => handleVacancyClick(vacancy)}
+            />
+          ))}
         </VacanciesContainer>
+        {selectedVacancy && <VacancyDetails vacancy={selectedVacancy}/>}
       </MainContent>
     </Container>
   )
