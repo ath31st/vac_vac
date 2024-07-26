5c43cfd9e575030e43e5f6c33daca5365d817332
ath31st <ath31st@yandex.ru>
2024-07-26 20:01:54 +0300

    add Sidebar.jsx

---
+++ frontend/src/components/button/SidebarButton.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const SidebarButton = styled.div`
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    text-align: center;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #f0f0f0;
+    }
+`
+export default SidebarButton
