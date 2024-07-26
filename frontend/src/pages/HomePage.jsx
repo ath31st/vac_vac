af37fd8fce03f31750d526a56495ea0e438c1ca1
ath31st <ath31st@yandex.ru>
2024-07-26 20:02:30 +0300

    rework HomePage.jsx add 2 buttons

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+import StyledLink from '../components/link/StyledLink'
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 60vh;
+`
+
+const Title = styled.h1`
+    margin-bottom: 20px;
+    color: #333;
+`
+
+const ButtonContainer = styled.div`
+    display: flex;
+    gap: 40px;
+`
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Get started with React-Router 6</h1>
-    </div>
+    <Container>
+      <Title>Welcome to the Job Search and Posting Service!</Title>
+      <ButtonContainer>
+        <StyledLink to="/login">Login</StyledLink>
+        <StyledLink to="/signup">Sign Up</StyledLink>
+      </ButtonContainer>
+    </Container>
   )
 }
 
