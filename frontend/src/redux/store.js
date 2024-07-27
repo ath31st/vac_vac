cfc3f70e9ec9a82d5fdae6be3c643398010e48c1
ath31st <ath31st@yandex.ru>
2024-07-27 09:24:21 +0300

    update store with middleware

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './authSlice'
+import authMiddleware from '../middleware/authMiddleware'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authMiddleware),
 })
 
 export default store
