f0043bb30af3bbdb0b9ed5068f6f2f07a32309fd
ath31st <ath31st@yandex.ru>
2024-07-27 08:53:45 +0300

    add SignUpContainer

---
+++ frontend/src/components/container/SignInUpContainer.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const SignInUpContainer = styled.div`
+    padding: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export default SignInUpContainer
