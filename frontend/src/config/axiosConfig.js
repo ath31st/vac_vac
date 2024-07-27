9f07fe83a1bcb7794f13c2e1a5721723cb4d8b2f
ath31st <ath31st@yandex.ru>
2024-07-27 09:48:30 +0300

    add axiosConfig.js

---
+++ frontend/src/config/axiosConfig.js
@@ -0,0 +1,37 @@
+// src/axiosConfig.js
+import axios from 'axios'
+import store from '../redux/store'
+import { performLogout } from '../redux/authSlice'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+const apiUrl = process.env.REACT_APP_API_BASE_URL
+
+axios.defaults.baseURL = apiUrl
+
+axios.interceptors.request.use(
+  config => {
+    const token = store.getState().auth.token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => Promise.reject(error),
+)
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    if (error.response && error.response.status === 401) {
+      dispatch(performLogout())
+      navigate('/login')
+    }
+    return Promise.reject(error)
+  },
+)
+
+export default axios
