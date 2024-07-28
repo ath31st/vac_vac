3268d8812c23ec816e0dbb6273b14582f33a41db
ath31st <ath31st@yandex.ru>
2024-07-28 11:06:57 +0300

    change return type from Vacancy to VacancyDto

--- backend/src/main/kotlin/doma/sidim/service/VacancyService.kt
+++ backend/src/main/kotlin/doma/sidim/service/VacancyService.kt
@@ -1,6 +1,8 @@
 package doma.sidim.service
 
+import doma.sidim.dto.TagDto
 import doma.sidim.dto.VacancyCreationDto
+import doma.sidim.dto.VacancyDto
 import doma.sidim.model.Vacancy
 import doma.sidim.repository.VacancyRepository
 import doma.sidim.util.EducationLevels
@@ -29,7 +31,18 @@ class VacancyService(private val vacancyRepository: VacancyRepository) {
         return vacancyRepository.update(id, vacancy)
     }
 
-    fun getAllActiveVacancies(): List<Vacancy> {
-        return vacancyRepository.getActiveVacancies()
+    fun getAllActiveVacancies(): List<VacancyDto> {
+        return vacancyRepository.getActiveVacancies().map {
+            VacancyDto(
+                it.id,
+                it.title,
+                it.description,
+                it.grade.toString(),
+                it.englishLevel.toString(),
+                it.tags.map { tag -> TagDto(tag.ordinal, tag.toString()) },
+                it.isVisible,
+                it.creatorId,
+            )
+        }
     }
 }
