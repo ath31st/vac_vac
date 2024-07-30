6eea606a0380c9d61e1d2cdae27b3ad889fd00c9
ath31st <ath31st@yandex.ru>
2024-07-30 23:06:55 +0300

    rework Vacancies page v3. add MyVacancies.jsx for employee

---
+++ frontend/src/pages/MyVacancies.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import Sidebar from '../components/Sidebar'
+import VacanciesList from '../components/VacanciesList'
+
+const Container = styled.div`
+    display: flex;
+`
+
+const MainContent = styled.div`
+    flex-grow: 1;
+    display: flex;
+`
+
+const MyVacancies = () => {
+  return (
+    <Container>
+      <Sidebar/>
+      <MainContent>
+        <VacanciesList endpoint="/api/v1/vacancies"/>
+      </MainContent>
+    </Container>
+  )
+}
+
+export default MyVacancies
