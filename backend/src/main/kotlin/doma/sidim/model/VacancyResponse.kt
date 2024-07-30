1622e0b6d58f6b9d5170068f8c45465cfa0e037e
ath31st <ath31st@yandex.ru>
2024-07-30 14:42:13 +0300

    update VacancyResponse.kt with VacancyResponse model

--- backend/src/main/kotlin/doma/sidim/model/VacancyResponse.kt
+++ backend/src/main/kotlin/doma/sidim/model/VacancyResponse.kt
@@ -1,6 +1,14 @@
 package doma.sidim.model
 
+import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Table
+
+@Serializable
+data class VacancyResponse(
+    val id: Long? = null,
+    val vacancyId: Long,
+    val userId: Long,
+)
 
 object VacancyResponses : Table("vacancy_responses") {
     val id = long("vacancy_response_id").autoIncrement()
