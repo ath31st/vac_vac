06b87ab8a53bff417bf5f10bee648aa9ee1a64db
ath31st <ath31st@yandex.ru>
2024-07-30 23:06:34 +0300

    rework Vacancies page v2. add ActiveVacancies.jsx for company User

---
+++ frontend/src/pages/ActiveVacancies.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+import Sidebar from '../components/Sidebar'
+import VacanciesList from '../components/VacanciesList'
+
+const Container = styled.div`
+    display: flex;
+`
+
+const MainContent = styled.div`
+    flex-grow: 1;
+    display: flex;
+`
+
+const ActiveVacancies = () => {
+  return (
+    <Container>
+      <Sidebar/>
+      <MainContent>
+        <VacanciesList endpoint="/api/v1/vacancies/active"/>
+      </MainContent>
+    </Container>
+  )
+}
+
+export default ActiveVacancies
