e774c06d76efd6a99da3348b3ddc72abd3351637
ath31st <ath31st@yandex.ru>
2024-08-01 11:32:36 +0300

    update VacanciesList.jsx, rework vacancy response display

--- frontend/src/components/VacanciesList.jsx
+++ frontend/src/components/VacanciesList.jsx
@@ -3,6 +3,7 @@ import Vacancy from '../components/Vacancy'
 import { useEffect, useState } from 'react'
 import axios from '../config/axiosConfig'
 import VacancyDetails from '../components/VacancyDetails'
+import { useSelector } from 'react-redux'
 
 const VacanciesContainer = styled.div`
     display: flex;
@@ -14,19 +15,66 @@ const VacanciesContainer = styled.div`
 const VacanciesList = ({ endpoint }) => {
   const [selectedVacancy, setSelectedVacancy] = useState(null)
   const [vacancies, setVacancies] = useState([])
+  const role = useSelector(state => state.auth.user?.role)
+  const userId = useSelector(state => state.auth.user?.user_id)
 
   useEffect(() => {
     const fetchVacancies = async () => {
       try {
         const response = await axios.get(endpoint)
         setVacancies(response.data)
+
+        setTimeout(() => {
+          if (role === 0) {
+            fetchResponseStatuses(response.data.map(vacancy => vacancy.id))
+          } else if (role === 1) {
+            fetchResponseCounts(
+              response.data
+              .filter(v => v.creatorId === userId)
+              .map(vacancy => vacancy.id))
+          }
+        }, 200)
       } catch (error) {
         console.error('Error fetching Vacancies:', error)
       }
     }
 
     fetchVacancies()
-  }, [endpoint])
+  }, [endpoint, role, userId])
+
+  const fetchResponseStatuses = async (vacancyIds) => {
+    try {
+      const response = await axios.post('/api/v1/vacancies/response-statuses',
+        vacancyIds)
+      const statuses = response.data
+
+      setVacancies((prevVacancies) =>
+        prevVacancies.map((vacancy) => ({
+          ...vacancy,
+          hasResponded: statuses[vacancy.id] || false,
+        })),
+      )
+    } catch (error) {
+      console.error('Error fetching response statuses:', error)
+    }
+  }
+
+  const fetchResponseCounts = async (vacancyIds) => {
+    try {
+      const response = await axios.post('/api/v1/vacancies/response-counts',
+        vacancyIds)
+      const counts = response.data
+
+      setVacancies((prevVacancies) =>
+        prevVacancies.map((vacancy) => ({
+          ...vacancy,
+          responseCount: counts[vacancy.id] || null,
+        })),
+      )
+    } catch (error) {
+      console.error('Error fetching response counts:', error)
+    }
+  }
 
   const fetchVacancyDetails = async (vacancyId) => {
     try {
@@ -58,6 +106,8 @@ const VacanciesList = ({ endpoint }) => {
             key={vacancy.id}
             title={vacancy.title}
             description={vacancy.description}
+            hasResponded={role === 0 ? vacancy.hasResponded : null}
+            responseCount={role === 1 ? vacancy.responseCount : null}
             onClick={() => handleVacancyClick(vacancy.id)}
           />
         ))}
