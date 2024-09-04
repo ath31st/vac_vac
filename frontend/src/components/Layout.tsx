94cf9a936f3b8bfe6763c3d8191eb230727f4571
ath31st <ath31st@yandex.ru>
2024-09-04 14:43:13 +0300

    rework Layout.tsx and PrivateRoute.tsx

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-// @ts-expect-error TS(6142): Module '../components/Header' was resolved to '/ho... Remove this comment to see the full error message
 import Header from '../components/Header';
-// @ts-expect-error TS(6142): Module '../components/Footer' was resolved to '/ho... Remove this comment to see the full error message
 import Footer from '../components/Footer';
 import styled from 'styled-components';
 
@@ -13,18 +11,13 @@ const Container = styled.main`
   flex-direction: column;
 `;
 
-const Layout = () => {
+const Layout: React.FC = () => {
   return (
-    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <Header />
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <Container>
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <Outlet />
       </Container>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <Footer />
     </>
   );
--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
-const PrivateRoute = ({
-  element
-}: any) => {
+interface PrivateRouteProps {
+  element: React.ReactElement;
+}
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({ element }) => {
   const { isAuthenticated } = useAuth();
 
-  // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
   return isAuthenticated ? element : <Navigate to="/login" />;
 };
 
