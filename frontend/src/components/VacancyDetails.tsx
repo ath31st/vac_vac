c0c695a0e091ffc717ee154526d769a7d71303f5
ath31st <ath31st@yandex.ru>
2024-09-04 14:44:13 +0300

    rework VacancyDetails.tsx

--- frontend/src/components/VacancyDetails.tsx
+++ frontend/src/components/VacancyDetails.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-// @ts-expect-error TS(6142): Module './button/SubmitButton' was resolved to '/h... Remove this comment to see the full error message
 import SubmitButton from './button/SubmitButton';
-// @ts-expect-error TS(6142): Module './message/ErrorMessage' was resolved to '/... Remove this comment to see the full error message
 import ErrorMessage from './message/ErrorMessage';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import { VacancyData } from '../types';
 
 const Container = styled.div`
   padding: 20px;
@@ -45,62 +45,72 @@ const ButtonContainer = styled.div`
   flex-direction: row-reverse;
 `;
 
-const VacancyDetails = ({
-  vacancy,
-  onVacancyChange
-}: any) => {
-  const [error, setError] = useState('');
-  // @ts-expect-error TS(2571): Object is of type 'unknown'.
-  const role = useSelector((state) => state.auth.user?.role);
-  // @ts-expect-error TS(2571): Object is of type 'unknown'.
-  const userId = useSelector((state) => state.auth.user?.user_id);
+interface VacancyDetailsProps {
+  vacancy: VacancyData;
+  onVacancyChange: (updatedVacancy: VacancyData) => void;
+  onRemoveVacancy: (vacancyId: string) => void;
+}
 
-  const handleResponseVacancy = async (vacancyId: any) => {
+const VacancyDetails: React.FC<VacancyDetailsProps> = ({
+  vacancy,
+  onVacancyChange,
+}) => {
+  const [error, setError] = useState<string>('');
+  const role = useSelector((state: RootState) => state.auth.user?.role);
+  const userId = useSelector((state: RootState) => state.auth.user?.user_id);
+
+  const handleResponseVacancy = async (vacancyId: string) => {
     try {
       const endpoint = `/api/v1/vacancies/${vacancyId}/response`;
       await axios.post(endpoint);
       onVacancyChange({ ...vacancy, hasResponded: true });
-    } catch (error) {
+    } catch (error: unknown) {
       console.log(error);
-      // @ts-expect-error TS(2571): Object is of type 'unknown'.
-      if (error.response && error.response.status === 409) {
+      if (
+        axios.isAxiosError(error) &&
+        error.response &&
+        error.response.status === 409
+      ) {
         setError('You have already responded to this vacancy');
       } else {
-        // @ts-expect-error TS(2571): Object is of type 'unknown'.
-        setError(error.response.data);
+        setError('An error occurred');
       }
     }
   };
 
-  const handleCancelResponseVacancy = async (vacancyId: any) => {
+  const handleCancelResponseVacancy = async (vacancyId: string) => {
     try {
       const endpoint = `/api/v1/vacancies/${vacancyId}/cancel-response`;
       await axios.delete(endpoint);
       onVacancyChange({ ...vacancy, hasResponded: false });
-    } catch (error) {
-      // @ts-expect-error TS(2571): Object is of type 'unknown'.
-      if (error.response && error.response.status === 404) {
+    } catch (error: unknown) {
+      if (
+        axios.isAxiosError(error) &&
+        error.response &&
+        error.response.status === 404
+      ) {
         setError('No response has been found for this vacancy');
       } else {
-        // @ts-expect-error TS(2571): Object is of type 'unknown'.
-        setError(error.response.data);
+        setError('An error occurred');
       }
     }
   };
 
-  const handleChangeVisibleVacancy = async (vacancy: any) => {
+  const handleChangeVisibleVacancy = async (vacancy: VacancyData) => {
     try {
       const isVisible = !vacancy.isVisible;
       const endpoint = `/api/v1/vacancies/${vacancy.id}/change-visible`;
-      await axios.put(endpoint, { isVisible: isVisible });
-      onVacancyChange({ ...vacancy, isVisible: isVisible });
-    } catch (error) {
-      // @ts-expect-error TS(2571): Object is of type 'unknown'.
-      if (error.response && error.response.status === 404) {
+      await axios.put(endpoint, { isVisible });
+      onVacancyChange({ ...vacancy, isVisible });
+    } catch (error: unknown) {
+      if (
+        axios.isAxiosError(error) &&
+        error.response &&
+        error.response.status === 404
+      ) {
         setError('Vacancy not found');
       } else {
-        // @ts-expect-error TS(2571): Object is of type 'unknown'.
-        setError(error.response.data);
+        setError('An error occurred');
       }
     }
   };
@@ -108,29 +118,20 @@ const VacancyDetails = ({
   const isUserCreator = vacancy.creatorId === userId;
 
   return (
-    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <Container>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <VacancyTitle>{vacancy.title}</VacancyTitle>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <EnglishGradeLvls>English lvl: {vacancy.englishLevel}</EnglishGradeLvls>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <EnglishGradeLvls>Grade: {vacancy.grade}</EnglishGradeLvls>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <VacancyDescription>{vacancy.description}</VacancyDescription>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <h3>Contacts</h3>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <Tags>
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
-        {vacancy.tags.map((tag: any) => <Tag key={tag.id}>{tag.name}</Tag>)}
+        {vacancy.tags.map((tag) => (
+          <Tag key={tag.id}>{tag.name}</Tag>
+        ))}
       </Tags>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       {error && <ErrorMessage>{error}</ErrorMessage>}
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <ButtonContainer>
         {role === 1 && isUserCreator ? (
-          // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
           <SubmitButton
             type="button"
             onClick={() => handleChangeVisibleVacancy(vacancy)}
@@ -138,10 +139,8 @@ const VacancyDetails = ({
             {vacancy.isVisible ? 'Close vacancy' : 'Open vacancy'}
           </SubmitButton>
         ) : role !== 1 ? (
-          // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
           <>
             {vacancy.hasResponded ? (
-              // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
               <SubmitButton
                 type="button"
                 onClick={() => handleCancelResponseVacancy(vacancy.id)}
@@ -149,7 +148,6 @@ const VacancyDetails = ({
                 Cancel response
               </SubmitButton>
             ) : (
-              // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
               <SubmitButton
                 type="button"
                 onClick={() => handleResponseVacancy(vacancy.id)}
