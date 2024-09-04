8780ce74acb411be01d85be2b21e09aee1c817a4
ath31st <ath31st@yandex.ru>
2024-09-04 14:42:28 +0300

    rework SelectField.tsx

--- frontend/src/components/select/SelectField.tsx
+++ frontend/src/components/select/SelectField.tsx
@@ -1,33 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Option as CustomOption } from '../../types';
 
 const StyledSelect = styled.select`
-    margin: 10px 0;
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
+  margin: 10px 0;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 `;
 
-const SelectField = ({
+interface SelectFieldProps {
+  id?: string;
+  name: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  options: CustomOption[];
+  placeholder?: string;
+}
+
+const SelectField: React.FC<SelectFieldProps> = ({
   id,
   name,
   value,
   onChange,
   options,
-  placeholder
-}: any) => {
+  placeholder,
+}) => {
   const selectId = id || name;
 
   return (
-    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <StyledSelect id={selectId} name={name} value={value} onChange={onChange}>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
-      <option value="" disabled>{placeholder}</option>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
-      {options.map((option: any) => <option key={option.id} value={option.id}>
-        {option.name}
-      </option>)}
+      <option value="" disabled>
+        {placeholder}
+      </option>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
     </StyledSelect>
   );
 };
