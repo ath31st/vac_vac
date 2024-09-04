109acb35b4b29767abd6f8f89e830067470d669c
ath31st <ath31st@yandex.ru>
2024-09-04 14:45:25 +0300

    migrate to ts HomePage.tsx page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-// @ts-expect-error TS(6142): Module '../components/link/StyledLink' was resolve... Remove this comment to see the full error message
 import StyledLink from '../components/link/StyledLink';
 
 const Container = styled.div`
@@ -21,17 +20,12 @@ const ButtonContainer = styled.div`
   gap: 40px;
 `;
 
-const HomePage = () => {
+const HomePage: React.FC = () => {
   return (
-    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <Container>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <Title>Welcome to the Job Search and Posting Service!</Title>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <ButtonContainer>
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <StyledLink to="/login">Login</StyledLink>
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <StyledLink to="/signup">Sign Up</StyledLink>
       </ButtonContainer>
     </Container>
