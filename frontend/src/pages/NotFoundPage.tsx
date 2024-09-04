02b3591f71dba7de25366bd41d4e42e8d97b351f
ath31st <ath31st@yandex.ru>
2024-09-04 14:46:23 +0300

    migrate to ts NotFoundPage.tsx

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFoundPage = () => {
+const NotFoundPage: React.FC = () => {
   return (
-    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <div>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
-      This page doesn't exist. Go <Link to="/">home</Link>
+      This page doesn`t exist. Go <Link to="/">home</Link>
     </div>
   );
 };
