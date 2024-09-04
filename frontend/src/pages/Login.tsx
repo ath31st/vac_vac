226cde788057618aa1fc4c25f23cceceee6fd9e5
ath31st <ath31st@yandex.ru>
2024-09-04 14:45:36 +0300

    migrate to ts Login.tsx page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// @ts-expect-error TS(6142): Module '../components/input/InputField' was resolv... Remove this comment to see the full error message
 import InputField from '../components/input/InputField';
-// @ts-expect-error TS(6142): Module '../components/button/SubmitButton' was res... Remove this comment to see the full error message
 import SubmitButton from '../components/button/SubmitButton';
-// @ts-expect-error TS(6142): Module '../components/message/ErrorMessage' was re... Remove this comment to see the full error message
 import ErrorMessage from '../components/message/ErrorMessage';
 import { Link, useNavigate } from 'react-router-dom';
 import { login } from '../redux/authSlice';
-// @ts-expect-error TS(6142): Module '../components/form/SignInUpForm' was resol... Remove this comment to see the full error message
 import SignInUpForm from '../components/form/SignInUpForm';
-// @ts-expect-error TS(6142): Module '../components/container/SignInUpContainer'... Remove this comment to see the full error message
 import SignInUpContainer from '../components/container/SignInUpContainer';
-// @ts-expect-error TS(6142): Module '../components/label/LabelWithLink' was res... Remove this comment to see the full error message
 import LabelWithLink from '../components/label/LabelWithLink';
+import { AppDispatch, RootState } from '../redux/store';
 
-const Login = () => {
+const Login: React.FC = () => {
   const [formState, setFormState] = useState({
     email: '',
     password: '',
   });
 
-  const dispatch = useDispatch();
-  // @ts-expect-error TS(2571): Object is of type 'unknown'.
-  const error = useSelector((state) => state.auth.error);
-  // @ts-expect-error TS(2571): Object is of type 'unknown'.
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch<AppDispatch>();
+  const error = useSelector((state: RootState) => state.auth.error);
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
   const navigate = useNavigate();
 
-  const handleChange = (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormState({
       ...formState,
@@ -36,10 +31,13 @@ const Login = () => {
     });
   };
 
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // @ts-expect-error TS(2345): Argument of type 'AsyncThunkAction<{ token: any; u... Remove this comment to see the full error message
-    dispatch(login(formState));
+    const formData = new FormData();
+    formData.append('email', formState.email);
+    formData.append('password', formState.password);
+
+    dispatch(login(formData));
   };
 
   useEffect(() => {
@@ -49,13 +47,9 @@ const Login = () => {
   }, [isAuthenticated, navigate]);
 
   return (
-    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <SignInUpContainer>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <SignInUpForm onSubmit={handleSubmit}>
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <h2>Login</h2>
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <InputField
           type="email"
           name="email"
@@ -63,7 +57,6 @@ const Login = () => {
           value={formState.email}
           onChange={handleChange}
         />
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <InputField
           type="password"
           name="password"
@@ -71,14 +64,10 @@ const Login = () => {
           value={formState.password}
           onChange={handleChange}
         />
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         {error && <ErrorMessage>{error}</ErrorMessage>}
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <SubmitButton type="submit">Sign In</SubmitButton>
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <LabelWithLink>
-          // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
-          Don't have an account? <Link to="/signup">Register here</Link>
+          Don`t have an account? <Link to="/signup">Register here</Link>
         </LabelWithLink>
       </SignInUpForm>
     </SignInUpContainer>
