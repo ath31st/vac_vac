d9ed3ef272d46ab1f37c0e5d1172b9cf226e6831
ath31st <ath31st@yandex.ru>
2024-09-04 14:46:00 +0300

    migrate to ts MyVacancies.tsx page

--- frontend/src/pages/MyVacancies.tsx
+++ frontend/src/pages/MyVacancies.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-// @ts-expect-error TS(6142): Module '../components/Sidebar' was resolved to '/h... Remove this comment to see the full error message
 import Sidebar from '../components/Sidebar';
-// @ts-expect-error TS(6142): Module '../components/VacanciesList' was resolved ... Remove this comment to see the full error message
 import VacanciesList from '../components/VacanciesList';
 
 const Container = styled.div`
@@ -14,15 +12,11 @@ const MainContent = styled.div`
   display: flex;
 `;
 
-const MyVacancies = () => {
+const MyVacancies: React.FC = () => {
   return (
-    // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     <Container>
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <Sidebar />
-      // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
       <MainContent>
-        // @ts-expect-error TS(17004): Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
         <VacanciesList endpoint="/api/v1/vacancies/employee" />
       </MainContent>
     </Container>
